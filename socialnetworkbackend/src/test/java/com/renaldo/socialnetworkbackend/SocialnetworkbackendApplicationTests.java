804724c5c96c66875239164bb9c4b2a908b3805a
sleepyren <96728203+sleepyren@users.noreply.github.com>
2024-06-11 21:54:19 -0400

    spring boot backend initial state

---
+++ socialnetworkbackend/src/test/java/com/renaldo/socialnetworkbackend/SocialnetworkbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renaldo.socialnetworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
